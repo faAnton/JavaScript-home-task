e1e1137b90b4044242897d0a0d7537c4d5b82c3b
faAnton <tonifedar@mail.ru>
2026-01-07 16:28:52 +0300

    second week home task complite

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,18 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 70// тестовое значение, можно изменять
 let grade
 // your code
+
+if(score >= 0 && score < 50) {
+    grade = 'F'
+}else if (score >= 50 && score < 70){
+    grade = 'D'
+}else if (score >= 70 && score < 80){
+    grade = 'C'
+}else if (score >= 80 && score < 90){
+    grade = 'B'
+}else if (score >= 90 && score < 101){
+    grade = 'A'
+}
